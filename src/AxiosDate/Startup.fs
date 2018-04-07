edc1b07da09e703a8329dc25db9f651f71410fe6
wk-j <somnuk.wk@gmail.com>
2018-04-07 20:33:50 +0700

    Removed commit

--- src/AxiosDate/Startup.fs
+++ src/AxiosDate/Startup.fs
@@ -1,12 +1,7 @@
 namespace AxiosDate
 
-open System
-open System.Collections.Generic
-open System.Linq
-open System.Threading.Tasks
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
@@ -16,13 +11,10 @@ type Startup private () =
         Startup() then
         this.Configuration <- configuration
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    member this.ConfigureServices(services: IServiceCollection) =
-        // Add framework services.
+    member __.ConfigureServices(services: IServiceCollection) =
         services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1) |> ignore
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
+    member __.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
         if (env.IsDevelopment()) then
             app.UseDeveloperExceptionPage() |> ignore
         else
